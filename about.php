cf0057669e07bdf199ac4a3855f668774593c896
DDHL97 <1715110005@utec-tgo.edu.mx>
2017-04-25 07:50:19 -0500

    Rename admin.php to about.php

---
+++ about.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+    <title>Pueblos Magicos del esatdo de Hidalgo</title>
+    <style>
+        #menu {
+            width: 200px;
+            float: right;
+        }
+    </style>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="static/css/bootstrap.css" rel="stylesheet">
+    <link href="static/css/bootstrap.min.css" rel="stylesheet" media="screen"> 
+    <link href="static/css/carrusel.css" rel="stylesheet">
+</head>
+<body>
+<nav role="navigation" class="navbar navbar-default">
+        <div class="navbar-header">
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                <span class="sr-only">Le Tournee</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="index.php" class="navbar-brand">Le Tournée</a>
+        </div>
+ 
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Inicio</a></li>
+                <li class="active"><a href="about.php">Acerca de</a></li>
+                <li><a href="login.php">Iniciar Sesión</a></li>
+            </ul>
+        </div>
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="col-md-5 col-md-offset-3">
+            <div class="page-header text center">
+                <h1>Acerca de</h1>       
+            </div>
+            <p>Esta aplicacion fue creada con la finalidad de ayudar a la publicidad de los pueblos magicos del estado de Hidalgo para que asi la ciudadania del mismo estado antes mencionado disfrute de todo la magia, misticismo, cultura, gastronomia y paisajes que los rodean sin tener que salir a otros estados de la Republica mexicana.</p>
+            <div align = "center">
+            <img src="static/imgweb/letournee.png" width="200" height="200"/>
+                </div>
+        </div>
+    </div>
+</div>
+</body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="static/js/bootstrap.min.js"></script>
+    <script src="http://code.jquery.com/jquery.js"></script>
+ 
+</html>
